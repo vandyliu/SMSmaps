ea8f2a213e72fbcf7b84732e183b4848da287fe4
liang liu <stuffbyliang@gmail.com>
2019-11-30 13:58:44 -0800

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+import json
+
+import googleapi.directions as googleapi
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/directions/")
+def directions():
+    mode = request.args.get('mode')
+    language = request.args.get('language')
+    arrival_time = request.args.get('arrival_time')
+    departure_time = request.args.get('departure_time')
+    origin = request.args.get('origin')
+    destination = request.args.get('destination')
+
+    return googleapi.directions(origin,
+                                destination,
+                                mode,
+                                language,
+                                arrival_time,
+                                departure_time)
