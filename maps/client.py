3d1ec97758691c610631a05bf90c941a401b78cd
Vandy Liu <33995460+vandyliu@users.noreply.github.com>
2019-11-30 16:45:57 -0800

    Added maps image (#5)

--- maps/client.py
+++ maps/client.py
@@ -2,6 +2,9 @@ import os
 import googlemaps
 from datetime import datetime
 import json
+import requests
+
+from maps import helpers as mh
 
 key = os.getenv("GOOGLE_API_KEY")
 gmaps = googlemaps.Client(key=key)
@@ -34,3 +37,28 @@ def instructions(res):
                 instructions.append(step2["html_instructions"])
 
     return '\n'.join(instructions)
+
+def map_image(res):
+    """Gets URL of a google maps image that shows the route
+    
+    Arguments:
+        res {dict} -- google maps response
+    
+    Returns:
+        string -- URL of maps image
+    """
+    # constants
+    MAP_URL = "https://maps.googleapis.com/maps/api/staticmap"
+    SIZE = "400x400"
+
+    polygon_path = mh.get_polygon_path(res)
+    origin = mh.get_latlon(mh.get_origin(res))
+    destination = mh.get_latlon(mh.get_destination(res))
+    params = {
+        "size": SIZE,
+        "path": f"enc:{polygon_path}",
+        "markers": [f"color:red|label:X|{destination}", f"size:small|color:blue|{origin}"],
+        "key": key
+    }
+    img_resp = requests.get(url=MAP_URL, params=params)
+    return img_resp.url
