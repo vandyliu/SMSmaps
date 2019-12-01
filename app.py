d980825e4d6fbcb95b6d70d3e067a4798ba492a3
EricJ111 <36346746+EricJ111@users.noreply.github.com>
2019-11-30 17:34:16 -0800

    remember cookies

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, session
 from dotenv import load_dotenv
 import json
 
@@ -32,7 +32,34 @@ def directions():
     return client.instructions(res)
 @app.route("/sms", methods=['GET', 'POST'])
 def reply():
-    return sms_reply()
+    # Increment the counter
+    # counter = session.get('counter', 0)
+    # counter += 1
+    # session['counter'] = counter
+
+    # callers = {
+    #     "+7783788024": "Friend",
+    #     "+6043524722": "Friend",
+    #     "+7788148834": "Friend",
+    # }
+
+    #Get number
+    from_number = request.values.get('From')
+    to_number = request.values.get('To')
+
+    # if from_number in callers:
+    #     name = callers[from_number]
+    # else:
+    #     name = "Friend"
+
+    return sms_reply('name', to_number, '2')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+SECRET_KEY = 'a secret key'
+app.config.from_object(__name__)
+
+
+    
