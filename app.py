f4b4d9956ebea541b4b67b9546a83ced29589d51
Liang Liu <stuffbyliang@gmail.com>
2019-11-30 17:18:32 -0800

    Googlemaps (#6)
    Added feature to obtain directions from location a to location b as a string

--- app.py
+++ app.py
@@ -15,7 +15,25 @@ app = Flask(__name__)
 def hello():
     return "hello world"
 
-@app.route("/instructions/")
+@app.route("/directions/raw/")
+def directions_raw():
+    mode = request.args.get('mode')
+    language = request.args.get('language')
+    arrival_time = request.args.get('arrival_time')
+    departure_time = request.args.get('departure_time')
+    origin = request.args.get('origin')
+    destination = request.args.get('destination')
+
+    res = client.directions(origin,
+                            destination,
+                            mode,
+                            language,
+                            arrival_time,
+                            departure_time)
+
+    return res
+
+@app.route("/directions/")
 def directions():
     mode = request.args.get('mode')
     language = request.args.get('language')
@@ -25,13 +43,24 @@ def directions():
     destination = request.args.get('destination')
 
     res = client.directions(origin,
-                               destination,
-                               mode,
-                               language,
-                               arrival_time,
-                               departure_time)
+                            destination,
+                            mode,
+                            language,
+                            arrival_time,
+                            departure_time)
+    parsed_data = client.parse(res)
+    directions = (
+        f'Departure: {parsed_data["departure_time"]}\n'
+        f'Arrival: {parsed_data["arrival_time"]}\n'
+        f'Total Trip Time: {parsed_data["duration"]}\n'
+        f'Total Trip Distance: {parsed_data["distance"]}\n'
+        f'Start Address: {parsed_data["start_address"]}\n'
+        f'Destination Address: {parsed_data["end_address"]}\n\n'
+    )
 
-    return client.instructions(res)
+    directions += client.get_instruction_string(parsed_data["steps"])
+
+    return directions
 
 @app.route("/sms", methods=['GET', 'POST'])
 def reply():
--- maps/client.py
+++ maps/client.py
@@ -6,11 +6,29 @@ import requests
 
 from maps import helpers as mh
 
+import re
+
 key = os.getenv("GOOGLE_API_KEY")
 gmaps = googlemaps.Client(key=key)
 
 def directions(origin, destination, mode, language, arrival_time, departure_time):
-    # Request directions via public transit    
+    """returns raw direction object from google maps directions api
+    
+    Arguments:
+        origin {string} -- Starting Location eg. Starbucks near UBC Bookstore
+        destination {string} -- Ending Location
+        mode {string} -- method of travel, defaults to "driving"
+                         "transit" "walking" "driving" or "bicycling"
+        language {string} -- language to return in, defaults to "EN". eg. "zh-CN", "fr-CA"
+                             see https://developers.google.com/maps/faq#languagesupport
+        arrival_time {integer} -- Specifies the desired time of arrival.
+                                  Seconds since midnight, January 1, 1970 UTC
+        departure_time {integer} -- Specifies the desired time of departure.
+                                    Seconds since midnight, January 1, 1970 UTC
+    
+    Returns:
+        [dict] -- [raw directions object]
+    """    
     now = datetime.now()
     res = gmaps.directions(origin,
                            destination,
@@ -25,18 +43,117 @@ def directions(origin, destination, mode, language, arrival_time, departure_time
         # empty
         return "Not Found"
     else:
-        return res
+        return res[0]
 
-def instructions(res):
-    instructions=[]
+def parse(res):
+    """converts raw direction dict from google maps api to readible format
+    
+    Arguments:
+        res {dict} -- raw direction dict from google maps api
+    
+    Returns:
+        [dict] -- readible directions format
+    """    
+    steps = []
+    lat = res["legs"][0]["end_location"]["lat"]
+    lng = res["legs"][0]["end_location"]["lng"]
 
-    for step in res[0]["legs"][0]["steps"]:
-        instructions.append(step["html_instructions"])
-        if "steps" in step:
-            for step2 in step["steps"]:
-                instructions.append(step2["html_instructions"])
+    for step in res["legs"][0]["steps"]:
+        instruction = re.sub('<[^<]+?>', '', step["html_instructions"])
+        distance = step["distance"]["text"]
+        duration = step["duration"]["text"]
 
-    return '\n'.join(instructions)
+        if step["travel_mode"] == "TRANSIT":
+            departure_stop = step["transit_details"]["departure_stop"]["name"]
+            arrival_stop = step["transit_details"]["arrival_stop"]["name"]
+            departure_time =  step["transit_details"]["departure_time"]["text"]
+            arrival_time =  step["transit_details"]["arrival_time"]["text"]
+            num_stops =  step["transit_details"]["num_stops"]
+            bus_name = step["transit_details"]["headsign"]
+
+            steps.append({
+                "distance": distance,
+                "duration": duration,
+                "instruction": instruction,
+                "bus_name": bus_name,
+                "num_stops": num_stops,
+                "arrival_time": arrival_time,
+                "departure_time": departure_time,
+                "departure_stop": departure_stop,
+                "arrival_stop": arrival_stop,
+                "travel_mode": "TRANSIT"
+            })
+        else:
+            substeps = []
+            if "steps" in step:
+                for step2 in step["steps"]:
+                    instruction2 = re.sub('<[^<]+?>', '', step2["html_instructions"])
+                    distance2 = step2["distance"]["text"]
+                    duration2 = step2["duration"]["text"]
+
+                    substeps.append({
+                        "distance": distance2,
+                        "duration": duration2,
+                        "instruction": instruction2
+                    })
+            steps.append({
+                "distance": distance,
+                "duration": duration,
+                "instruction": instruction,
+                "substeps": substeps,
+                "travel_mode": step2["travel_mode"]
+            })
+
+    return {
+        "arrival_time": res["legs"][0].get("arrival_time", {}).get("text", None),
+        "departure_time": res["legs"][0].get("departure_time", {}).get("text", None),
+        "end_address": res["legs"][0]["end_address"],
+        "start_address": res["legs"][0]["start_address"],
+        "distance": res["legs"][0]["distance"]["text"],
+        "duration": res["legs"][0]["duration"]["text"],
+        "destination_url": f'http://www.google.com/maps/place/{lat},{lng}',
+        "steps": steps,
+    }
+
+def get_instruction_string(steps, show_substeps=False):
+    """Converts steps dict to string
+    
+    Arguments:
+        steps {dict} -- steps dict
+    
+    Returns:
+        [string] -- Stringified version of instructions
+    """    
+
+    string = ""
+    n = 1
+    for step in steps:
+        instruction = ""
+        if step["travel_mode"] == "TRANSIT":
+            instruction = (
+                f'Step {n}: At {step["departure_time"]} take the {step["instruction"]}\n'
+                f'Bus Name: {step["bus_name"]}\n'
+                f'Get off stop {step["departure_stop"]} at {step["arrival_time"]} after riding {step["num_stops"]} stops\n'
+                f'\n'
+            )
+            string += instruction
+        else:
+            instruction = (
+                f'Step {n}: {step["instruction"]}\n'
+                f'Approx {step["duration"]} and {step["distance"]}\n'
+                f'\n'
+            )
+            string += instruction
+
+            if show_substeps:
+                for step2 in step["substeps"]:
+                    instruction = (
+                        f'{step2["instruction"]}\n'
+                    )
+                    string += instruction + '\n'
+        n += 1
+    
+    return string
 
 def map_image(res):
     """Gets URL of a google maps image that shows the route
